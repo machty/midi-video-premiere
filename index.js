bf46279186dab1266568ec7407f3d94b7ea211c6
Alex Matchneer <machty@gmail.com>
2021-11-29 00:00:58 -0500

    get markers from clips

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 /// <reference types="types-for-adobe/Premiere/2018"/>
-function map(array, callback) {
-    var out = [];
-    for (var index = 0; index < array.length; index++) {
-        out.push(callback(array[index]));
-    }
-    return out;
-}
-$.writeln(map([1, 2, 3], function (a) { return 123; }));
+// type ArrayIshOf<K> = {
+//     length: number;
+// };
+// function map<K, lengthProp>(array: ArrayIshOf<K>, callback) : K[] {
+//     const out = [];
+//     for (let index = 0; index < array[lengthProp]; index++) {
+//         out.push(callback(array[index]));
+//     }
+//     return out;
+// }
 var trackNr = 0;
 var clipNr = 0;
 var seq = app.project.activeSequence;
 var videoTracks = seq.videoTracks;
 var videoTrack = videoTracks[trackNr];
+var clips = videoTrack.clips;
 var videoClip = videoTrack.clips[clipNr];
+// $.writeln(`the clips`)
+// $.writeln(
+//     map(videoTrack.clips, c => c.name)
+// )
 // Get a list of all the clips that are in the project panel
 var availableClips = app.project.rootItem.children;
-var clip0 = availableClips[0];
-var a = 123;
-var b = a + 123;
-// $.writeln("HAHAHA123");
+var clip = availableClips[2];
+var markers = clip.getMarkers();
+var marker = markers[1];
+$.writeln(marker.name);
 debugger;
+// videoTrack.insertClip(clip, 0);
+// videoTrack.insertClip(trackNr, trackNr);
--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 /// <reference types="types-for-adobe/Premiere/2018"/>
 
-function map(array, callback) {
-    const out = [];
-    for (let index = 0; index < array.length; index++) {
-        out.push(callback(array[index]));
-    }
-    return out;
-}
+// type ArrayIshOf<K> = {
+//     length: number;
+// };
 
-$.writeln(
-    map([1,2,3], a => 123)
-)
+// function map<K, lengthProp>(array: ArrayIshOf<K>, callback) : K[] {
+//     const out = [];
+//     for (let index = 0; index < array[lengthProp]; index++) {
+//         out.push(callback(array[index]));
+//     }
+//     return out;
+// }
 
 const trackNr = 0;
 const clipNr = 0;
@@ -18,17 +18,23 @@ const clipNr = 0;
 const seq = app.project.activeSequence;
 const videoTracks = seq.videoTracks;
 const videoTrack = videoTracks[trackNr];
+const clips = videoTrack.clips;
 const videoClip = videoTrack.clips[clipNr];
+
+// $.writeln(`the clips`)
+// $.writeln(
+//     map(videoTrack.clips, c => c.name)
+// )
 
 // Get a list of all the clips that are in the project panel
 const availableClips = app.project.rootItem.children;
-const clip0 = availableClips[0];
-
-const a = 123;
-const b = a + 123;
-
-// $.writeln("HAHAHA123");
+const clip = availableClips[2];
+const markers = clip.getMarkers();
+const marker: Marker = markers[1];
+$.writeln(
+    marker.name
+);
 
 debugger;
-
-
+// videoTrack.insertClip(clip, 0);
+// videoTrack.insertClip(trackNr, trackNr);
